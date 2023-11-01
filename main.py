6aa71fce6f5e861fcf507de90f09c8aef685adfe
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2023-11-01 19:45:44 -0400

    conv

--- main.py
+++ main.py
@@ -2,10 +2,24 @@ from PIL import Image, ImageFilter
 import sys
 import os
 
-#creates new directory
-current = os.getcwd()
-parent_director = f'{current}'
-directory = sys.argv[2]
-path = os.path.join(parent_director, directory)
-os.mkdir(path)
 
+def main():
+    current = os.getcwd()  # get current working dir
+    directory = sys.argv[2]  # gets the name of new directory from user using sys
+    target_directory = sys.argv[1]
+    create_directory(current, directory)
+    convert_image(f'{current}\\{target_directory}', f'{current}\\{directory}')
+
+
+def convert_image(source_path, destination_path):
+    with Image.open(source_path) as img:
+        img.save(destination_path, "PNG")
+
+
+def create_directory(c, d):
+    path = os.path.join(c, d)
+    os.mkdir(path)  # uses os to create the new directory
+
+
+if __name__ == '__main__':
+    main()
