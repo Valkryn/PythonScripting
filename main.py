3fdfd7d42a9b504548dfb59c0dd37428878c2e3d
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2023-11-01 19:06:17 -0400

    creates new dir

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-def main():
-    pass
+from PIL import Image, ImageFilter
+import sys
+import os
+
+#creates new directory
+current = os.getcwd()
+parent_director = f'{current}'
+directory = sys.argv[2]
+path = os.path.join(parent_director, directory)
+os.mkdir(path)
+
