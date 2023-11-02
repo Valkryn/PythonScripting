ab186f60987eddf0ae413048133ade01ad2c1388
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2023-11-01 20:38:45 -0400

    mvp

---
+++ PngConverter/main.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import sys
+import os
+
+
+def main():
+    output_folder = sys.argv[2]  # gets the name of new directory from user using sys
+    image_folder = sys.argv[1]
+    create_directory(output_folder)
+    all_img = os.listdir(f'{os.getcwd()}\\{image_folder}')
+    convert_image(all_img, image_folder, output_folder)
+
+
+def create_directory(d):
+    if not os.path.exists(d):
+        os.mkdir(d)  # uses os to create the new directory
+
+
+def convert_image(img_list, image_folder, output_folder):
+    for img_name in img_list:
+        img = Image.open(f'{os.getcwd()}\\{image_folder}\\{img_name}')
+        clean_name = os.path.splitext(img_name)[0]
+        img.save(f'{os.getcwd()}\\{output_folder}\\{clean_name}.png', 'png')
+
+
+if __name__ == '__main__':
+    main()
