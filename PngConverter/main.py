764fa0c5c362471effaa8a549675627871dbf152
Calvin Gutierrez <61283082+Valkryn@users.noreply.github.com>
2023-11-02 11:51:56 -0400

    added notes and self to do list

--- PngConverter/main.py
+++ PngConverter/main.py
@@ -12,14 +12,14 @@ def main():
 
 
 def create_directory(d):
-    if not os.path.exists(d):
+    if not os.path.exists(d): #if not - if the file DOES NOT EXIST ; create it.
         os.mkdir(d)  # uses os to create the new directory
 
 
 def convert_image(img_list, image_folder, output_folder):
     for img_name in img_list:
         img = Image.open(f'{os.getcwd()}\\{image_folder}\\{img_name}')
-        clean_name = os.path.splitext(img_name)[0]
+        clean_name = os.path.splitext(img_name)[0] #splits the name of the from the format [0]=name [1]=format
         img.save(f'{os.getcwd()}\\{output_folder}\\{clean_name}.png', 'png')
 
 
